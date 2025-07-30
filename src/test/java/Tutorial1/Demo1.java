6dd541bd64913b401187e737d1b5ae1f8eee963c
Owner <Owner@MSI>
2025-07-30 17:01:55 +0530

    Home TC by Tester1

--- src/test/java/Tutorial1/Demo1.java
+++ src/test/java/Tutorial1/Demo1.java
@@ -5,6 +5,9 @@ public class Demo1 {
 	public static void main(String[] args) {
 		
 		System.out.println("BDD framework is created");
+		System.out.println("Home Page Test Case by Tester1");
 	}
+	
+	
 
 }
